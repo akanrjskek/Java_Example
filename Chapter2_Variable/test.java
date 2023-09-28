43754f0750eeefd4cccd13a36eefeedd290081a4
akanrjskek <akanrjskek@naver.com>
2023-09-29 00:54:18 +0900

    202309290055

--- Chapter2_Variable/test.java
+++ Chapter2_Variable/test.java
@@ -3,6 +3,10 @@ package Chapter2_Variable;
 public class test {
     public static void main(String[] args) {
         System.out.println("test");
+<<<<<<< HEAD
         System.out.println("hello");
+=======
+        System.out.println();
+>>>>>>> f9e9515 (202309290055)
     }
 }
