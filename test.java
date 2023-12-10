0786780911430b2a9795ce2e7028a6a4cffb6162
Yeonghun Won <akanrjskek@naver.com>
2023-12-10 22:23:18 +0900

    231210

--- test.java
+++ test.java
@@ -1,21 +1,11 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.Scanner;
-
+import java.util.List;
+import java.util.Arrays;
 public class test {
     public static void main(String[] args) throws Exception{
-        StringBuilder sb = new StringBuilder();
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());
-        for(int i = 0; i < n; ++i){
-            for(int j = 0; j < n - 1 - i; ++j){
-                sb.append(" ");
-            }
-            for(int j = 0; j < i + 1; ++j){
-                sb.append("*");
-            }
-            sb.append("\n");
-        }
-        System.out.println(sb);
+
     }
 }
