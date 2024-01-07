c87b575a2393c2842fb8acf9ddb1b7847586e9b7
Yeonghun Won <akanrjskek@naver.com>
2024-01-07 23:56:15 +0900

    230108

--- Main.java
+++ Main.java
@@ -4,13 +4,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-
-        int[] a;
-        a = list.stream().mapToInt(i->i).toArray();
-        System.out.println(Arrays.toString(a));
-
+        System.out.println("hello");
     }
 }
