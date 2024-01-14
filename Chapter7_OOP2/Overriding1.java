023e9d7df13100cf168488b7ca76de2d8e0d3e6c
Yeonghun Won <akanrjskek@naver.com>
2024-01-15 00:35:59 +0900

    240115

---
+++ Chapter7_OOP2/Overriding1.java
@@ -0,0 +1,20 @@
+package Chapter7_OOP2;
+
+class H{
+    public void method(){
+        System.out.println("H class");
+    }
+}
+
+class I extends H{
+    public void method(int a){
+        System.out.println("I class");
+    }
+}
+
+
+public class Overriding1 {
+    public static void main(String[] args) {
+
+    }
+}
