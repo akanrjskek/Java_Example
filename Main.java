5ca90f69a1027935bea9ba28f18e3f450de613df
akanrjskek <akanrjskek@naver.com>
2024-01-14 18:45:01 +0900

    240114

--- Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello2");
-    }
-}
