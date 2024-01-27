6a521b30fbaf2a25dcd51e49e17a0c1c7d710ddb
Yeonghun Won <akanrjskek@naver.com>
2024-01-27 17:21:12 +0900

    240127

--- Main.java
+++ Main.java
@@ -1,18 +1,10 @@
-class A {
-    void method(int a){
-        System.out.println("a = " + a);
-    }
-}
-
-class B extends A {
-    void method(int a, int b){
-        System.out.println("a = " + a);
-    }
+interface Ainterface{
+    Ainterface(){}
 }
 
 
 public class Main {
     public static void main(String[] args){
-        A a = new B();
+        Ainterface a = new Ainterface();
     }
 }
