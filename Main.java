7aab145d645a6912f6c2d9841bf54173ba0184c1
Yeonghun Won <akanrjskek@naver.com>
2024-03-26 11:29:01 +0900

    240326

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        String s = null;
-        System.out.println("length of s == " + s.length());
+        char c = Character.MAX_VALUE;
+
     }
 }
